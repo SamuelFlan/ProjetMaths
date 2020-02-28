dfa7f94eb48c58870029dcd2d45cb75a72b060c0
Kahel <micaoa@gmail.com>
2020-02-28 18:10:48 +0100

    fix CalculerDistance

--- voyageur-de-commerce/src/Heuristique.java
+++ voyageur-de-commerce/src/Heuristique.java
@@ -38,6 +38,6 @@ public class Heuristique {
 	
 	//retourne la distance euclidienne entre deux villes
 	public static double CalculerDistance(Ville a, Ville b) {
-		return Math.sqrt(Math.sqrt((b.x-a.x))+Math.sqrt(b.y-a.y));
+		return Math.sqrt(Math.pow((b.x-a.x),2)+Math.pow((b.y-a.y),2));
 	}
 }
