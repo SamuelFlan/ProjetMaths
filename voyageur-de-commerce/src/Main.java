84a1c50556fc53f6d6f1c52c5a9364b101b4e908
LeBohec Laetitia <39592050+SamuelFlan@users.noreply.github.com>
2020-03-05 10:45:43 +0100

    Changement du main
    Changement du main afin d'implanter la classe InOut

--- voyageur-de-commerce/src/Main.java
+++ voyageur-de-commerce/src/Main.java
@@ -11,52 +11,10 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Ville> villes = LireVillesDeFichier("jeu-essai/20.in3");
+		InOut i = new InOut();
+		ArrayList<Ville> villes = i.LireVillesDeFichier("jeu-essai/20.in3");
 		Solution solution = Heuristique.ParAjoutDuPlusProche(villes);
-		EcrireSolutionDansFichier(solution, "jeu-essai/20.out3");
-	}
-	
-	//retourne la liste du fichier <nomFichier>
-	public static ArrayList<Ville> LireVillesDeFichier(String nomFichier){
-    	ArrayList<Ville> villes = new ArrayList<Ville>();
-	    try {
-	        File fichier = new File(nomFichier);
-	        Scanner scanner = new Scanner(fichier).useLocale(Locale.US);
-	        
-        	//on saute la premiere ligne (nombre de ville)
-        	scanner.nextInt();
-	        while (scanner.hasNextDouble()) {
-				double x, y;
-				x = scanner.nextDouble();
-				y = scanner.nextDouble();
-				villes.add(new Ville(x, y));
-	        }
-	        scanner.close();
-	      } catch (FileNotFoundException e) {
-	        System.out.println("Fichier "+nomFichier+" non trouvÃ©");
-	        e.printStackTrace();
-	      }
-        return villes;
-	}
-	
-	//écrit la solution obtenue dans un fichier
-	public static void EcrireSolutionDansFichier(Solution solution, String nomFichier) {
-        try {
-        	BufferedWriter writer = new BufferedWriter(new FileWriter(nomFichier));
-            //on écrit le nombre de ville
-            writer.write(Integer.toString(solution.parcours.size()));
-            //la longueur du parcours
-            writer.newLine();
-            writer.write(Double.toString(solution.longueur));
-            //et les coordonnées des villes du parcours
-            for(Ville ville : solution.parcours) {
-            	writer.newLine();
-            	writer.write(ville.toString());
-            }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		i.EcrireSolutionDansFichier(solution, "jeu-essai/20.out3");
 	}
 	
 	
